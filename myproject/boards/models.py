a614f678c46b4c69c30e0719094ddbb9069c777d
dym0080 <308960474@qq.com>
2019-12-23 17:26:24 +0800

    myproject--part2

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# on_delete 在2.0后是必选参数。
+
+class Board(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=100)
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=300)
+    last_update = models.DateField(auto_now_add=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+class Post(models.Model):
+    message = models.TextField(max_length=5000)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.SET_NULL)
