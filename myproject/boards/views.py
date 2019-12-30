1ca9f40e72d647d74d7111f82c9eb7e9e54a3969
dym0080 <308960474@qq.com>
2019-12-30 17:00:43 +0800

    update myproject --part5 post reply

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
 
 # Create your views here.
 
@@ -39,7 +39,20 @@ def new_topic(request, pk):
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     posts = Post.objects.filter(topic_id=topic_pk)
-    print(11)
-    print(posts)
-    print(12)
     return render(request, 'boards/topic_posts.html', {'topic':topic, 'posts':posts})
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    posts = Post.objects.filter(topic_id=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form =PostForm()
+    return render(request, 'boards/reply_topic.html', {'topic': topic, 'posts': posts, 'form': form})
