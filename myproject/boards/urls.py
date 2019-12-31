865971770efcdfe6a266a66d1e5fa39f6928eec1
dym0080 <308960474@qq.com>
2019-12-31 16:22:53 +0800

    update my project --part6 Pagination

--- myproject/boards/urls.py
+++ myproject/boards/urls.py
@@ -5,7 +5,7 @@ from . import views
 # app_name = 'boards'
 urlpatterns =[
     path('', views.BoardListView.as_view(), name='home'),
-    path('<int:pk>/', views.board_topics, name='board_topics'),
+    path('<int:pk>/', views.TopicListView.as_view(), name='board_topics'),
     path('<int:pk>/new/', views.new_topic, name='new_topic'),
     path('<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
     path('<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView, ListView
@@ -18,10 +19,33 @@ class BoardListView(ListView):
     template_name = 'boards/home.html'
     context_object_name = 'boards'
 
-def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    topics = board.topics.order_by('-last_update').annotate(replies=Count('posts') - 1)
-    return render(request, 'boards/topics.html', {'board':board, 'topics':topics})
+# def board_topics(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     queryset = board.topics.order_by('-last_update').annotate(replies=Count('posts') - 1)
+#     page = request.GET.get('page', 1)
+#     paginator = Paginator(queryset, 20)
+#     try:
+#         topics = paginator.page(page)
+#     except PageNotAnInteger:
+#         topics = paginator.page(1)
+#     except EmptyPage:
+#         topics = paginator.page(paginator.num_pages)
+#     return render(request, 'boards/topics.html', {'board':board, 'topics':topics})
+
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'boards/topics.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_update').annotate(replies=Count('posts') - 1)
+        return queryset
 
 @login_required
 def new_topic(request, pk):
