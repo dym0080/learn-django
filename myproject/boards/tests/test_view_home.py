59fd639b98864fdc5ea5d8c830501db1e26ddd52
dym0080 <308960474@qq.com>
2019-12-31 15:14:36 +0800

    update myproject --part 6

--- myproject/boards/tests/test_view_home.py
+++ myproject/boards/tests/test_view_home.py
@@ -1,7 +1,7 @@
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
 from django.test import TestCase
-from ..views import board_topics, new_topic
+from ..views import board_topics, new_topic, BoardListView
 from ..models import Board, Topic, Post
 from ..forms import NewTopicForm
 
@@ -10,13 +10,14 @@ class HomeTests(TestCase):
         self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('home')
         self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         self.assertEqual(self.response.status_code, 200)
-    
-    # def test_home_url_resolves_home_view(self):
-    #     view = resolve('/')
-    #     self.assertEqual(view.func, home)
 
     def test_home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
         self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+    
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/boards/')
+        self.assertEqual(view.func.view_class, BoardListView)
--- myproject/boards/urls.py
+++ myproject/boards/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 # app_name = 'boards'
 urlpatterns =[
-    path('', views.home, name='home'),
+    path('', views.BoardListView.as_view(), name='home'),
     path('<int:pk>/', views.board_topics, name='board_topics'),
     path('<int:pk>/new/', views.new_topic, name='new_topic'),
     path('<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
     path('<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
+    path('<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/',
+        views.PostUpdateView.as_view(), name='edit_post'),
     # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 ]
